ff534980ddf44f206adc5abae707337becf6aa25
Productive Programmer <charles.lobo@gmail.com>
2018-09-05 00:10:56 +1000

    Add message handler registry

--- index.js
+++ index.js
@@ -69,26 +69,108 @@ function startMicroservice(cfg) {
      * appropriate requests.
      *
      *      way/
-     * The skill registers itself which allows the communication manager
-     * to send communications to it.
+     * Anything that can handle messages registers itself which allows
+     * the communication manager to send it user communication to which
+     * it can respond if it knows how.
      */
-    let skillRegistry = []
-    commMgrSvc.on('register-skill', (req, cb) => {
-        skillRegistry.push(req)
-        cb(null)
-    })
+    commMgrSvc.on('register-msg-handler', registerMsgHandler)
 
 
     /*      outcome/
-     * The user has sent a message so send a reply
+     * The user has sent a message so we see if any component can handle
+     * it otherwise we reply that we didn't understand.
      */
     commMgrSvc.on('message', (req, cb) => {
-        sendReply(req.chan, {
-            type: 'reply',
-            ctx: req.ctx,
-            msg: 'Manager says: ' + req.msg,
-        }, cb)
+        if(!req.chan) cb(`Request missing channel! ${req}`)
+        else {
+            if(!req.ctx) cb(`Request missing context! ${req}`)
+            else {
+                if(!req.msg) cb()
+                else handleReply(req, (err, handling) => {
+                    // TODO: Error messaging (especially object dumps) need to
+                    // be designed better
+                    if(err) cb(`Error! ${err}`)
+                    else {
+                        if(handling) cb()
+                        else sendReply(`I'm sorry - I did not understand: ${req.msg}`, req, cb)
+                    }
+                })
+            }
+        }
     })
+
+    /*      outcome/
+     * We have a reply for the user so we call the correct channel with
+     * the reply
+     */
+    commMgrSvc.on('reply', (req, cb) => {
+        if(!req.chan) cb(`Request missing channel! ${req}`)
+        else {
+            if(!req.ctx) cb(`Request missing context! ${req}`)
+            else {
+                if(!req.msg) cb()
+                else sendReply(req.msg, req, cb)
+            }
+        }
+    })
+}
+
+let msgHandlerRegistry = []
+function registerMsgHandler(req, cb) {
+    if(!req.mskey || !req.mstype) cb(`mskey & mstype needed to register msg handler`)
+    else {
+        let client = new cote.Requester({
+            name: `CommMgr -> ${req.mskey}`,
+            key: req.mskey,
+        })
+        msgHandlerRegistry.push({client: client, mstype: req.mstype})
+        cb(null)
+    }
+}
+
+/*      understand/
+ * The user can install multiple skills and many of these extend the
+ * bot's abilitiy to respond to new commands, conversations, and so on.
+ *
+ *      outcome/
+ * If the user has been interacting with a particular skill, it gets the
+ * first chance of responding (continuing the conversation). If not, we
+ * go through all registered skills and see if any of them respond.
+ *
+ * TODO: Note that this implies that we can only handle/respond to one
+ * user.
+ */
+let CURRENT_HANDLER
+function handleReply(req, cb) {
+    if(CURRENT_HANDLER) {
+        isHandling(CURRENT_HANDLER, req, (err,handling) => {
+            if(err) u.showErr(err)
+            else {
+                if(handling) cb(null, true)
+                else check_handler_ndx_1(0)
+            }
+        })
+    } else check_handler_ndx_1(0)
+
+    function check_handler_ndx_1(ndx) {
+        if(ndx >= msgHandlerRegistry.length) cb()
+        else {
+            isHandling(msgHandlerRegistry[ndx], req, (err,handling) => {
+                if(err) u.showErr(err)
+                else {
+                    if(handling) {
+                        CURRENT_HANDLER = msgHandlerRegistry[ndx]
+                        cb(null, true)
+                    } else check_handler_ndx_1(ndx+1)
+                }
+            })
+        }
+    }
+}
+
+function isHandling(skill, req, cb) {
+    req.type = skill.mstype
+    skill.client.send(req, cb)
 }
 
 
@@ -103,11 +185,15 @@ let workq = new cote.Requester({
  * Use the queue microservice to properly stack messages for each
  * channel.
  */
-function sendReply(chan, rep, cb) {
+function sendReply(msg, req, cb) {
     workq.send({
         type: 'q',
-        q: chan,
-        data: rep,
+        q: req.chan,
+        data: {
+            type: 'reply',
+            ctx: req.ctx,
+            msg: msg,
+        },
     }, cb)
 }
 
